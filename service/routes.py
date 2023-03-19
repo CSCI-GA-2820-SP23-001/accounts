d432ad105b4ede5f7945e91d4f2e6a0eb625cbd4
John Rofrano <johnnyroy@johnrofrano.com>
2023-03-19 17:32:36 +0000

    Added endpoint to create an account

--- service/routes.py
+++ service/routes.py
@@ -28,4 +28,50 @@ def index():
 #  R E S T   A P I   E N D P O I N T S
 ######################################################################
 
-# Place your REST API code here ...
+
+######################################################################
+# CREATE An ACCOUNT
+######################################################################
+@app.route("/accounts", methods=["POST"])
+def create_accounts():
+    """
+    Creates an Account
+    This endpoint will create an Account based the data in the body that is posted
+    """
+    app.logger.info("Request to create an account")
+    check_content_type("application/json")
+    account = Account()
+    account.deserialize(request.get_json())
+    account.create()
+    message = account.serialize()
+
+    # TODO: Uncomment when get_accounts is implemented and delete "unknown"
+
+    # location_url = url_for("get_accounts", account_id=account.id, _external=True)
+    location_url = "unknown"
+
+    app.logger.info("Account with ID [%s] created.", account.id)
+    return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+
+
+def check_content_type(content_type):
+    """Checks that the media type is correct"""
+    if "Content-Type" not in request.headers:
+        app.logger.error("No Content-Type specified.")
+        abort(
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            f"Content-Type must be {content_type}",
+        )
+
+    if request.headers["Content-Type"] == content_type:
+        return
+
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {content_type}",
+    )
